e3a1b04172a9bdd387dc895387be1f1167bf35e4
anggr <anggorookky@gmail.com>
2022-12-16 10:07:43 +0800

    add leaderboard feature

--- routes/gameRouter.js
+++ routes/gameRouter.js
@@ -4,6 +4,6 @@ const game = require('../controllers/gameController');
 router.get('/', game.findAll);
 router.post('/', game.newGame);
 router.get('/:id', game.findOne);
-
+router.get('/:id/leaderboard', game.getLeaderboard);
 
 module.exports = router;
